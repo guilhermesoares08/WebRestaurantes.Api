dbe17aac9b540aeeca9538aa4afd88fa9cac28a4
Guilherme Soares <guilhermesoares1996@hotmail.com>
2021-09-20 13:21:05 -0300

    adicionada camada de repository da entidade Table

---
+++ WebRestaurantes.Repository/Repositories/TableRepository.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using WebRestaurantes.Domain;
+
+namespace WebRestaurantes.Repository
+{
+    public class TableRepository : BaseRepository<Table>, ITableRepository
+    {
+        private readonly WebRestaurantesContext _webRestaurantesContext;
+        public TableRepository(WebRestaurantesContext webRestaurantesContext) : base(webRestaurantesContext)
+        {
+            _webRestaurantesContext = webRestaurantesContext;
+            _webRestaurantesContext.ChangeTracker.QueryTrackingBehavior = QueryTrackingBehavior.NoTracking;
+        }
+    }
+}
